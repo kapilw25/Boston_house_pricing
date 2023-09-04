7b70fbf99b9d3af27b2293149bb5bed815fde789
Kapil Wanaskar <kapilw25@gmail.com>
2023-09-03 19:59:38 -0700

    modified:   Linear_Regression_ML_implementation.ipynb         modified:   app.py

--- app.py
+++ app.py
@@ -10,11 +10,24 @@ scalar=pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html') #render the html page
 
 @app.route('/predict_api',methods=['POST']) #post = send data to server via POSTMAN 
 
 def predict_api():
     data=request.json['data']
-    print(data)
+    print(data) # data is in key value pair
+    print(np.array(list(data.values())).reshape(1,-1))  #convert data into array
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1)) #transform data into scalar
+    output = regmodel.predict(new_data) #predict the data
+    print(output[0])
+    return jsonify(output[0]) #return the output in json format 
+
+if __name__ == "__main__":
+    app.run(debug=True) # run the app in debug mode, so that we can see the error in browser
+    
+    
+    
+     
+    
 
